5976f341348d510e8c4c4406eed31d32566f7121
ABC <d.bodkhe21@gmail.com>
2019-09-03 16:19:50 +0530

    test commit

--- app/src/main/java/com/example/abc/ballot/student_homepage.java
+++ app/src/main/java/com/example/abc/ballot/student_homepage.java
@@ -3,11 +3,11 @@ package com.example.abc.ballot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class student_homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_student_homepage);
     }
 }
