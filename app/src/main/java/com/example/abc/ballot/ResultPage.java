a9df72b4a44ff986b9473217729bd44dd8396a36
jadhavkajal631 <jadhavkajal631@gmail.com>
2019-10-06 00:09:07 +0530

    student_homepage.java mai added 1 comment as kajal

--- app/src/main/java/com/example/abc/ballot/ResultPage.java
+++ app/src/main/java/com/example/abc/ballot/ResultPage.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ResultPage extends AppCompatActivity {
+    //Kajal
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
