7744c8b53e041bf1c16c8af400fe7f85ed6d4911
jadhavkajal631 <jadhavkajal631@gmail.com>
2019-10-12 12:13:24 +0530

    all changes done on 9th oct

--- app/src/main/java/com/example/abc/ballot/Add_ElectionFragment.java
+++ app/src/main/java/com/example/abc/ballot/Add_ElectionFragment.java
@@ -1,16 +1,11 @@
 package com.example.abc.ballot;
-
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
-
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.media.RatingCompat;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,18 +13,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.NotNull;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -39,7 +29,7 @@ import static android.support.constraint.Constraints.TAG;
 
 
 //Teacher can add multiple elections
-public class Add_ElectionFragment extends Fragment{
+public class Add_ElectionFragment extends Fragment {
     View view;
     Button addpostbtn;
     EditText election_title;
@@ -47,7 +37,14 @@ public class Add_ElectionFragment extends Fragment{
     RadioButton class_radioButton;
     CalendarView calendarView;
     TextView tvselect;
+    Calendar mycal=Calendar.getInstance();
+    boolean TimePicker=false;
 
+    int hours,min;
+//time
+private TimePickerDialog time;
+Button btn_set_time;
+TextView tvselecttime;
 
     private DatePicker datePicker;
     private Calendar calendar;
@@ -69,63 +66,74 @@ public class Add_ElectionFragment extends Fragment{
     DatabaseReference database_list_reff;
 
     DatePickerDialog.OnDateSetListener date;
-    Calendar mycal;
-
-
-    public Add_ElectionFragment() {
-    }
-
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.add_election_fragment,container,false);
+        view = inflater.inflate( R.layout.add_election_fragment, container, false );
 
-        election_title = view.findViewById(R.id.edit_title_id);
+        election_title = view.findViewById( R.id.edit_title_id );
 
-        start_uid = view.findViewById(R.id.start_uid_id);
-        end_uid = view.findViewById(R.id.end_uid_id);
+        start_uid = view.findViewById( R.id.start_uid_id );
+        end_uid = view.findViewById( R.id.end_uid_id );
 
 
-        if(getArguments().getString("dept_n") != null)
-        {
-            dept = getArguments().getString("dept_n");
+        hours = mycal.get( Calendar.HOUR );
+        min = mycal.get( Calendar.MINUTE );
+
+
+        if (getArguments( ).getString( "dept_n" ) != null) {
+            dept = getArguments( ).getString( "dept_n" );
         }
 
-        Toast.makeText(getActivity(), dept, Toast.LENGTH_SHORT).show();
+        Toast.makeText( getActivity( ), dept, Toast.LENGTH_SHORT ).show( );
 
-        class_radiogrp = view.findViewById(R.id.radiogrp_class_id);
-        class_radiogrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        class_radiogrp = view.findViewById( R.id.radiogrp_class_id );
+        class_radiogrp.setOnCheckedChangeListener( new RadioGroup.OnCheckedChangeListener( ) {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                class_radioButton = view.findViewById(checkedId);
-                checked_class = class_radioButton.getText().toString().trim();
+                class_radioButton = view.findViewById( checkedId );
+                checked_class = class_radioButton.getText( ).toString( ).trim( );
 
             }
-        });
-        tvselect=view.findViewById(R.id.TVSelectedDate);
-        mycal=Calendar.getInstance();
+        } );
+        tvselect = view.findViewById( R.id.TVSelectedDate );
 
-        date=new DatePickerDialog.OnDateSetListener( ) {
-        @Override
-        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-            mycal.set(Calendar.YEAR,year );
-            mycal.set(Calendar.MONTH,month);
-            mycal.set(Calendar.DAY_OF_MONTH,day);
-            updateLabel();
 
-        }
-    };
+        date = new DatePickerDialog.OnDateSetListener( ) {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                mycal.set( Calendar.YEAR, year );
+                mycal.set( Calendar.MONTH, month );
+                mycal.set( Calendar.DAY_OF_MONTH, day );
+                updateLabel( );
+
+            }
+        };
         btn_set_date = view.findViewById( R.id.set_date_tbn_id );
         btn_set_date.setOnClickListener( new View.OnClickListener( ) {
             @Override
             public void onClick(View view) {
-                new DatePickerDialog( getContext(), date , mycal.get( Calendar.YEAR), mycal.get( Calendar.MONTH), mycal.get( Calendar.DAY_OF_MONTH)).show();
+                new DatePickerDialog( getContext( ), date, mycal.get( Calendar.YEAR ), mycal.get( Calendar.MONTH ), mycal.get( Calendar.DAY_OF_MONTH ) ).show( );
             }
         } );
-
-
+//        //timepicker 11 oct 2019
+        time=new TimePickerDialog( getContext( ), new TimePickerDialog.OnTimeSetListener( ) {
+            @Override
+            public void onTimeSet(android.widget.TimePicker timePicker, int hours, int min) {
+                hours=mycal.get(Calendar.HOUR);
+                min=mycal.get( Calendar.MINUTE );
+                updateLabe2();
+            }
+        },hours,min,false);
+        btn_set_time=view.findViewById( R.id.set_time_tbn_id );
+        tvselecttime=view.findViewById( R.id.TVSelectedTime );
+        btn_set_time.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick(View view) {
+              tvselecttime.setText( "Selected time: "+hours+" : "+min );
+            }
+        } );
 
         addpostbtn = view.findViewById(R.id.addpost_btn);
 
@@ -150,6 +158,12 @@ public class Add_ElectionFragment extends Fragment{
 
         return view;
     }
+
+    private void updateLabe2() {
+        String timeform="HOURS:MIN";
+        tvselecttime.setText( "Selected Time: "+hours+" : "+min );
+    }
+
     private void updateLabel() {
         String myFormat = "MM/dd/yy";
         SimpleDateFormat sdf = new SimpleDateFormat( myFormat, Locale.US);
